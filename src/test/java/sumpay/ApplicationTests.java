43a8c60b5a5d365a0c8102d725f4f3dcbc00b09d
Emmy Steven <hello@emmysteven.com>
2023-11-01 12:38:31 +0100

    initial commit

---
+++ src/test/java/sumpay/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sumpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
